4bcaa6b0b9f31d8f75f043886817f7949e5da55a
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-10 01:47:58 +0200

    Add user create test

--- go.mod
+++ go.mod
@@ -2,12 +2,12 @@ module github.com/quantumsheep/plouf/example
 
 go 1.18
 
-require github.com/labstack/echo/v4 v4.7.2
-
 require (
 	github.com/joho/godotenv v1.4.0
-	github.com/quantumsheep/plouf v0.0.0-20220409224749-62045dcf2bf2
-	github.com/stretchr/testify v1.7.0
+	github.com/labstack/echo/v4 v4.7.2
+	github.com/quantumsheep/plouf v0.0.0-20220409234447-b2f7de07be99
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.1
 	gorm.io/gorm v1.23.4
 )
 
@@ -19,23 +19,19 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	gorm.io/driver/sqlite v1.3.1 // indirect
-)
-
-require (
-	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/sirupsen/logrus v1.8.1
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gorm.io/driver/sqlite v1.3.1 // indirect
 )
--- modules/users/users_test.go
+++ modules/users/users_test.go
@@ -1,26 +1,78 @@
 package modules_users
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/quantumsheep/plouf"
+	modules_users_dto "github.com/quantumsheep/plouf/example/modules/users/dto"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUsersControllerGetById(t *testing.T) {
-	usersController := &UsersController{}
-	assert.NoError(t, plouf.Inject(usersController))
+func TestUsersControllerCreate(t *testing.T) {
+	os.Setenv("database_driver", "sqlite")
+	os.Setenv("database_path", ":memory:")
 
-	e := echo.New()
+	usersModule := &UsersModule{}
+	worker, err := plouf.NewWorkerMock(usersModule)
+	assert.NoError(t, err)
 
-	req := httptest.NewRequest(http.MethodGet, "/users/1", nil)
+	dto, err := json.Marshal(modules_users_dto.CreateUserBodyDTO{
+		Username: "John Doe",
+	})
+	assert.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(string(dto)))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	c := worker.NewContext(req, rec)
 
-	if assert.NoError(t, usersController.GetById(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
+	if assert.NoError(t, usersModule.UsersController.CreateUser(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+	}
+}
+
+func TestUsersControllerGetCreateUser(t *testing.T) {
+	os.Setenv("database_driver", "sqlite")
+	os.Setenv("database_path", ":memory:")
+
+	usersModule := &UsersModule{}
+	worker, err := plouf.NewWorkerMock(usersModule)
+	assert.NoError(t, err)
+
+	// Create user
+	{
+		dto, err := json.Marshal(modules_users_dto.CreateUserBodyDTO{
+			Username: "John Doe",
+		})
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(string(dto)))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := worker.NewContext(req, rec)
+
+		if assert.NoError(t, usersModule.UsersController.CreateUser(c)) {
+			assert.Equal(t, http.StatusCreated, rec.Code)
+		}
+	}
+
+	// Fetch created user (ID = 1)
+	{
+		req := httptest.NewRequest(http.MethodGet, "/users/1", nil)
+		rec := httptest.NewRecorder()
+		c := worker.NewContext(req, rec)
+		c.SetPath("/users/:id")
+		c.SetParamNames("id")
+		c.SetParamValues("1")
+
+		if assert.NoError(t, usersModule.UsersController.GetById(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
 	}
 }
